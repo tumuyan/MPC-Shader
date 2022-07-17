88a6ff0e8ef5983ce9928a09c2d5edffaa4c0d71
tumuyan <tumuyan@gmail.com>
2022-07-17 22:17:51 +0800

    Update 竖屏转横屏-中心强调.hlsl
    格式化代码

--- 竖屏转横屏-中心强调.hlsl
+++ 竖屏转横屏-中心强调.hlsl
@@ -15,49 +15,47 @@ float4 p1 : register(c1);
 #define PI acos(-1)
 #define angleSteps 12
 #define radiusSteps 10
-#define minRadius (0.0/width)
-#define maxRadius (10.0/width)
+#define minRadius (0.0 / width)
+#define maxRadius (10.0 / width)
 #define ampFactor 2.0
 
-float4 main(float2 tex : TEXCOORD0) : COLOR
-{
-	if (tex.x >= 0.5 - 0.5 / scale && tex.x <= 0.5 + 0.5 / scale) {
-	float2 scalefactor = float2(scale,1);
-	float2 newtex = (tex - float2(0.5,0.5)) * scalefactor + float2(0.5,0.5);
-	return tex2D(s0,newtex);
-	}
-	else {
+float4 main(float2 tex : TEXCOORD0) : COLOR {
+  if (tex.x >= 0.5 - 0.5 / scale && tex.x <= 0.5 + 0.5 / scale) {
+    float2 scalefactor = float2(scale, 1);
+    float2 newtex = (tex - float2(0.5, 0.5)) * scalefactor + float2(0.5, 0.5);
+    return tex2D(s0, newtex);
+  } else {
 
-	float2 scalefactor = float2(1,1.0 / scale);
-	float2 newtex = (tex - float2(0.5,0.5)) * scalefactor + float2(0.5,0.5);
-	// return tex2D(s0,newtex);
+    float2 scalefactor = float2(1, 1.0 / scale);
+    float2 newtex = (tex - float2(0.5, 0.5)) * scalefactor + float2(0.5, 0.5);
+    // return tex2D(s0,newtex);
 
-	float4 c0 = tex2D(s0, newtex);
-	float4 origColor = tex2D(s0, newtex);
-	float4 accumulatedColor = {0,0,0,0};
+    float4 c0 = tex2D(s0, newtex);
+    float4 origColor = tex2D(s0, newtex);
+    float4 accumulatedColor = {0, 0, 0, 0};
 
-	int totalSteps = radiusSteps * angleSteps;
-	float angleDelta = (2 * PI) / angleSteps;
-	float radiusDelta = (maxRadius - minRadius) / radiusSteps;
+    int totalSteps = radiusSteps * angleSteps;
+    float angleDelta = (2 * PI) / angleSteps;
+    float radiusDelta = (maxRadius - minRadius) / radiusSteps;
 
-	for (int radiusStep = 0; radiusStep < radiusSteps; radiusStep++) {
-		float radius = minRadius + radiusStep * radiusDelta;
+    for (int radiusStep = 0; radiusStep < radiusSteps; radiusStep++) {
+      float radius = minRadius + radiusStep * radiusDelta;
 
-		for (float angle = 0; angle < (2 * PI); angle += angleDelta) {
-			float2 currentCoord;
+      for (float angle = 0; angle < (2 * PI); angle += angleDelta) {
+        float2 currentCoord;
 
-			float xDiff = radius * cos(angle);
-			float yDiff = radius * sin(angle);
+        float xDiff = radius * cos(angle);
+        float yDiff = radius * sin(angle);
 
-			currentCoord = newtex + float2(xDiff, yDiff);
-			float4 currentColor = tex2D(s0, currentCoord);
-			float currentFraction = ((float)(radiusSteps + 1 - radiusStep)) / (radiusSteps + 1);
+        currentCoord = newtex + float2(xDiff, yDiff);
+        float4 currentColor = tex2D(s0, currentCoord);
+        float currentFraction =
+            ((float)(radiusSteps + 1 - radiusStep)) / (radiusSteps + 1);
 
-			accumulatedColor += currentFraction * currentColor / totalSteps;
+        accumulatedColor += currentFraction * currentColor / totalSteps;
+      }
+    }
 
-		}
-	}
-
-	return accumulatedColor * ampFactor;
-	}
+    return accumulatedColor * ampFactor;
+  }
 }
